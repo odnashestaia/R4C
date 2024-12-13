6353aedc5bb83f175abd9a6a91be9edd6ef900c2
odnashestaia <odna_shestaia@mail.ru>
2024-12-13 17:52:33 +0300

    fix some bugs

--- robots/services.py
+++ robots/services.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 from django.db.models import Count
 from django.utils.timezone import make_aware, utc
@@ -7,11 +7,15 @@ from .models import Robot
 
 
 def get_robot_summary():
-    end_date = datetime.datetime.now()
+    end_date = datetime.datetime.now().replace(
+        hour=23, minute=59, second=59, microsecond=999999)
     start_date = end_date - datetime.timedelta(days=7)
 
-    start_date = make_aware(start_date, utc)
-    end_date = make_aware(end_date, utc)
+    # Приведение к naive datetime
+    start_date = make_aware(start_date, timezone=utc)
+    end_date = make_aware(end_date, timezone=utc)
+    print(start_date, end_date)
+    print([i.created for i in Robot.objects.all()])
     return (
         Robot.objects.filter(created__range=[start_date, end_date])
         .values("model", "version")
@@ -23,5 +27,6 @@ def create_robot_in_db(validated_data):
     return Robot.objects.create(
         model=validated_data["model"],
         version=validated_data["version"],
-        created=validated_data["created"]
+        created=validated_data["created"],
+        serial=f"{validated_data["model"]}-{validated_data["version"]}"
     )
